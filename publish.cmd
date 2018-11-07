87c6ead452ec941ded67004278410dcd1c48f0eb
MarcelRuoff <marcel.ruoff@gmx.de>
2018-11-07 19:16:46 +0100

    Inital commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=affectivfeedbackbotv13-Web-Deploy.pubxml -p:Password=ibpY6PrmNLwY1DqhRWXZBrboyGe2WACi2DPbhg2Kz9xrgGhLXdNNonCrJ5en
+
